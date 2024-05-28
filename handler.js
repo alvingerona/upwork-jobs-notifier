e27999f6c8a912eb9360a29b8e7e466743c97b84
Alvin <geronaalvin@gmail.com>
2024-05-28 14:00:55 +0800

    display notifier function

--- handler.js
+++ handler.js
@@ -3,5 +3,7 @@
 // Import the jobsNotifierHandler function from the jobsNotifierHandler module
 import jobsNotifierHandler from "./src/handlers/jobsNotifier";
 
+const blankFunctionHandler = async () => {};
+
 // Export the jobsNotifierHandler function as jobsNotifierHandler
-export { jobsNotifierHandler };
+export { jobsNotifierHandler, blankFunctionHandler };
