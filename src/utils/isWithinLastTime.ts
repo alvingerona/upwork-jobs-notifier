123df2f629ec1bd1577535ae9c2d73bb4b7c59ce
Alvin <geronaalvin@gmail.com>
2024-05-29 14:00:34 +0800

    Improve time checker

--- src/utils/isWithinLastTime.ts
+++ src/utils/isWithinLastTime.ts
@@ -4,9 +4,8 @@ import { QUERY_INTERVAL } from "./constants";
 /**
  * Checks if a given moment is within the last specified time interval.
  *
- * This function calculates the difference between the current time and the
- * provided time (`momentToCheck`). If this difference is less than or equal
- * to the `QUERY_INTERVAL` (in minutes), the function returns true.
+ * This function determines whether the provided time (`momentToCheck`) falls
+ * within the last `QUERY_INTERVAL` minutes from the current time.
  *
  * @param {moment.Moment} momentToCheck - The time to check against the current time.
  * @returns {boolean} - True if the time difference is within the QUERY_INTERVAL, otherwise false.
@@ -14,12 +13,11 @@ import { QUERY_INTERVAL } from "./constants";
 function isWithinLastTime(momentToCheck: moment.Moment) {
   // Get the current time
   const now = moment();
-
-  // Calculate the difference in minutes
-  const diffInMinutes = now.diff(momentToCheck, "minutes");
+  // Get a certain time ago
+  const aTimeAgo = moment().subtract(QUERY_INTERVAL, "minutes");
 
   // Check if the difference is less than or equal to QUERY_INTERVAL minutes
-  const isValid = diffInMinutes <= QUERY_INTERVAL;
+  const isValid = momentToCheck.isBetween(aTimeAgo, now);
 
   return isValid;
 }
